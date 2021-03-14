35fd6b84b25b1f575d240de200daec814be1703e
shunsong <yushunsong@126.com>
2021-03-14 15:48:31 +0800

    新增android对接高德定位

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,13 @@
-import * as React from 'react';
+import React,{useEffect,useState} from 'react';
 import {View,Text} from 'react-native';
 import useThemeContext from '../../hooks/useThemeContext'
+import { getCurrentLocation } from '../../components/RNAmapGeolocation'
 const Home =()=>{
   const {theme}=useThemeContext()
   const {colors,icons}=theme
   return (
     <View>
-      <Text>Home</Text>
+      <Text></Text>
     </View>
   )
 }
